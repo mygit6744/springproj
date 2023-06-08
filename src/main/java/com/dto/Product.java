5676f2cfd5a1eda187450b159deff75ac70bdc29
Yugandhar Nalabolu <yugamfu@gmail.com>
2023-06-08 15:31:03 +0530

    modified on june eight

---
+++ src/main/java/com/dto/Product.java
@@ -0,0 +1,28 @@
+package com.dto;
+
+public class Product {
+
+	private Integer prodid;
+	private String prodname;
+	private Integer price;
+	public Integer getProdid() {
+		return prodid;
+	}
+	public void setProdid(Integer prodid) {
+		this.prodid = prodid;
+	}
+	public String getProdname() {
+		return prodname;
+	}
+	public void setProdname(String prodname) {
+		this.prodname = prodname;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+	
+}
